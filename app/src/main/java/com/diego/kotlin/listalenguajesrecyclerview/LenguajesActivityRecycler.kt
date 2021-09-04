9ba1833a8048ad05d166a4a3e922d916f0f0b95a
balay <dlolhd@gmail.com>
2021-09-04 09:38:02 +0200

    Commit implementación SwipeRefreshLayout + RecyclerView por el lado de la interfaz

--- app/src/main/java/com/diego/kotlin/listalenguajesrecyclerview/LenguajesActivityRecycler.kt
+++ app/src/main/java/com/diego/kotlin/listalenguajesrecyclerview/LenguajesActivityRecycler.kt
@@ -3,9 +3,9 @@ package com.diego.kotlin.listalenguajesrecyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LenguajesActivityRecycler : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lista_lenguajes_recy)
     }
 }
