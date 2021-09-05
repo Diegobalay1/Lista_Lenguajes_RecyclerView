3b6f7723c253e1a3e3a8aaf513464cd4262936b7
balay <dlolhd@gmail.com>
2021-09-05 08:51:04 +0200

    Commit de la interfaz individual de la lista 'item_lenguaje.xml'

--- app/src/main/java/com/diego/kotlin/listalenguajesrecyclerview/LenguajesActivityRecycler.kt
+++ app/src/main/java/com/diego/kotlin/listalenguajesrecyclerview/LenguajesActivityRecycler.kt
@@ -3,6 +3,12 @@ package com.diego.kotlin.listalenguajesrecyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Para utilizar un RecyclerView deberemos proporcionar los siguientes elementos:
+ * . Adaptador propio que vincule nuestros datos con la interfaz.
+ * . Layout manager que indique la distribución de los elementos en la lista.
+ * . Decoradores y animaciones de forma opcional.
+ */
 class LenguajesActivityRecycler : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
